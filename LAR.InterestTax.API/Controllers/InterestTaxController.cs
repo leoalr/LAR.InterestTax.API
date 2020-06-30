00992aa432dbc109dac3a306d6790abaf7ae5cda
Leo A. Ramos <41794683+leoalr@users.noreply.github.com>
2020-06-30 17:08:50 -0300

    First commit
    First commit to upload the first changes made.

---
+++ LAR.InterestTax.API/Controllers/InterestTaxController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LAR.InterestTax.API.Controllers
+{
+    /// <summary>
+    /// This controller is responsible for providing the Interest Tax resource
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InterestTaxController : ControllerBase
+    {
+        /// <summary>
+        /// Method to get the interest tax value
+        /// </summary>
+        /// <returns> a decimal parameter representing the interest tax value</returns>
+        [HttpGet] public decimal Get()
+        {
+            return 0.01M;
+        }
+    }
+}
